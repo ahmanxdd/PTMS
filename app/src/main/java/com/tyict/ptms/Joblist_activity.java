95a91a162282b34471fd08c0987ae49fff7f5960
Lee <omlhllkyr@gmail.com>
2015-07-04 22:58:33 +0800

    Merge branch 'master' of /Users/Lee/AndroidStudioProjects/PTMS with conflicts.

--- app/src/main/java/com/tyict/ptms/Joblist_activity.java
+++ app/src/main/java/com/tyict/ptms/Joblist_activity.java
@@ -1,29 +1,21 @@
 package com.tyict.ptms;
 
-import android.app.Activity;
-import android.database.DataSetObserver;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.ListView;
-
-import com.tyict.ptms.dataInfo.DatabaseView;
 
 public class Joblist_activity extends Fragment {
     View rootView;
-    ListView lvTable;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.joblist_layout, container, false);
         return rootView;
     }
-
 }
