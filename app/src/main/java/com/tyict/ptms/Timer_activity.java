9f4b015758574b46602b8ecebe1faa20e9957689
Lee <omlhllkyr@gmail.com>
2015-07-04 15:39:57 +0800

    7_4

---
+++ app/src/main/java/com/tyict/ptms/Timer_activity.java
@@ -0,0 +1,154 @@
+package com.tyict.ptms;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.text.format.Time;
+import android.os.Handler;
+
+import java.util.concurrent.TimeUnit;
+
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+@SuppressLint("NewApi")
+public class Timer_activity extends Activity implements View.OnClickListener {
+
+    TextView tvShowStartTime;
+    TextView tvTimer;
+    TextView tvShowEndTime;
+    TextView tvTotal;
+    Button btnStart;
+    Button btnEnd;
+    Button btnReset;
+    Button btnPause;
+    int s;
+    int m;
+    int h;
+    CounterClass timer;
+    boolean isEnd = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timer_layout);
+        tvTimer = (TextView) findViewById(R.id.tvTimer);
+        tvShowStartTime = (TextView) findViewById(R.id.tvShowStartTime);
+        tvShowEndTime = (TextView) findViewById(R.id.tvShowEndTime);
+        tvTotal = (TextView) findViewById(R.id.tvTotal);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        btnPause = (Button) findViewById(R.id.btnPause);
+        btnEnd = (Button) findViewById(R.id.btnEnd);
+
+        btnStart.setOnClickListener(this);
+        btnPause.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
+        btnEnd.setOnClickListener(this);
+
+        timer = new CounterClass(180000, 1000);
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    @SuppressLint("NewApi")
+    public class CounterClass extends CountDownTimer {
+
+        public CounterClass(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @SuppressLint("NewApi")
+        @Override
+        public void onTick(long millisUntilFinished) {
+           /* String hsm = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+            //tvTimer.setText(hsm);*/
+            s += 1;
+            if (s == 60) {
+                m += 1;
+                s = 0;
+            }
+            if (m == 60) {
+                m = 0;
+                s += 1;
+            }
+            tvTimer.setText(getTotalTimer());
+        }
+
+        @Override
+        public void onFinish() {
+            tvTimer.setText("00:00:00");
+        }
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStart:
+                timer.start();
+                Time timeStart = new Time();
+                timeStart.setToNow();
+                if (isEnd) {
+                    tvShowStartTime.setText("Start Time: " + timeStart.hour + ":" + timeStart.minute);
+                    tvShowEndTime.setText("End Time");
+                    isEnd = false;
+                    tvTotal.setText("");
+                }
+                break;
+            case R.id.btnPause:
+                timer.cancel();
+                break;
+            case R.id.btnReset:
+                isEnd = true;
+                timer.cancel();
+                s = 0;
+                m = 0;
+                h = 0;
+                tvTotal.setText("");
+                tvTimer.setText("00:00:00");
+                tvShowEndTime.setText("End Time:");
+                tvShowStartTime.setText("Start Time:");
+                break;
+            case R.id.btnEnd:
+                timer.cancel();
+                tvTotal.setText("Total: " + getTotalTimer());
+                s = 0;
+                m = 0;
+                h = 0;
+                Time timeEnd = new Time();
+                timeEnd.setToNow();
+                tvTimer.setText("00:00:00");
+                if (!isEnd){
+                    tvShowEndTime.setText("End Time: " + timeEnd.hour + ":" + timeEnd.minute);
+                    isEnd = true;
+                }
+                break;
+        }
+    }
+
+    protected String getTotalTimer() {
+        String total = "";
+        if (h < 10)
+            total += "0" + h + ":";
+        else
+            total += h + ":";
+        if (m < 10)
+            total += "0" + m + ":";
+        else
+            total += m + ":";
+        if (s < 10)
+            total += "0" + s;
+        else
+            total += s;
+
+        return total;
+    }
+}
