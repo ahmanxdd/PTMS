14b2fe344d7b347ca0636cc5a79c8a36c78b03a4
ahmanxdd <ahmanxdd@gmail.com>
2015-07-12 13:09:34 +0800

    BIG UI CHANGE

--- app/src/main/java/com/tyict/ptms/Login.java
+++ app/src/main/java/com/tyict/ptms/Login.java
@@ -1,13 +1,19 @@
 package com.tyict.ptms;
+
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ActionMenuView;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import com.tyict.ptms.dataInfo.DatabaseView;
 import com.tyict.ptms.dataInfo.LoginControl;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -15,67 +21,84 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-public class Login extends ActionBarActivity {
+public class Login extends ActionBarActivity
+{
 
     private static boolean opened = false;
     private DatabaseView dbv = new DatabaseView();
+    private EditText loginID;
+    private EditText loginPass;
 
-
-    private Login getThis() {
+    private Login getThis()
+    {
         return this;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
-        if (NonStoppable.lc.isLogin()) {
+        if (NonStoppable.lc.isLogin())
+        {
+
             startActivity(NonStoppable.i);
             this.finish();
         }
-
-
         setContentView(R.layout.activity_login);
+
+        loginID = ((EditText) findViewById(R.id.et_loginID));
+        loginPass = ((EditText) findViewById(R.id.et_password));
+        loginID.setText("login1001");
+        loginPass.setText("pass1001");
     }
 
 
-    public void btn_login_click(View v) {
-        String userID = ((EditText) findViewById(R.id.et_loginID)).getText().toString();
-        String password = ((EditText) findViewById(R.id.et_password)).getText().toString();
-        userID = "login1001";
-        password = "pass1001";
-        findViewById(R.id.et_loginID).setEnabled(false);
-        findViewById(R.id.et_password).setEnabled(false);
-        if (NonStoppable.lc.login(userID, password)) {
+    public void btn_login_click(View v)
+    {
+
+        String userID = loginID.getText().toString();
+        String password = loginPass.getText().toString();
+        loginID.setEnabled(false);
+        loginPass.setEnabled(false);
+        if (NonStoppable.lc.login(userID, password))
+        {
             NonStoppable.i = new Intent(this, A_Entry.class);
             new getJobList().execute(LoginControl.getStaffID());
             NonStoppable.opened = true;
-        } else {
+        } else
+        {
             ((TextView) findViewById(R.id.message)).setText("Login Failure, check your password and loginID");
-            ((EditText) findViewById(R.id.et_loginID)).setEnabled(true);
-            ((EditText) findViewById(R.id.et_password)).setEnabled(true);
+            loginID.setEnabled(true);
+            loginPass.setEnabled(true);
         }
     }
 
-    private class getJobList extends AsyncTask<String, Integer, String> {
+    private class getJobList extends AsyncTask<String, Integer, String>
+    {
 
         @Override
-        protected void onPostExecute(String s) {
+        protected void onPostExecute(String s)
+        {
             getThis().finish();
             startActivity(NonStoppable.i);
         }
 
         @Override
-        protected String doInBackground(String... staffNo) {
+        protected String doInBackground(String... staffNo)
+        {
 
             HttpClient client = new DefaultHttpClient();
             HttpGet request;
 
-            try {
+            try
+            {
                 String recvStr;
                 String url = "http://itd-moodle.ddns.me/ptms/service_job.php?staffNo=" + staffNo[0];
                 request = new HttpGet(url);
@@ -83,7 +106,8 @@ public class Login extends ActionBarActivity {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
                 String t;
                 recvStr = reader.readLine();
-                while ((t = reader.readLine()) != null) {
+                while ((t = reader.readLine()) != null)
+                {
                     recvStr += t;
                 }
 
@@ -94,7 +118,8 @@ public class Login extends ActionBarActivity {
                         " visitDate date, jobStatus text, jobStartTime text, jobEndTime text, serialNo text, remark text);";
                 //         dbv.exec(sql);
                 JSONArray json = new JSONObject(recvStr).getJSONArray("ServiceJob");
-                for (int j = 0; j < json.length(); j++) {
+                for (int j = 0; j < json.length(); j++)
+                {
                     JSONObject jo = json.getJSONObject(j);
                     String query = "INSERT INTO ServiceJob VALUES(" + "'" +
                             jo.getString("jobNo") + "','" +
@@ -111,9 +136,11 @@ public class Login extends ActionBarActivity {
                     getThis().finish();
 
                 }
-            } catch (IOException e) {
+            } catch (IOException e)
+            {
 
-            } catch (JSONException e) {
+            } catch (JSONException e)
+            {
 
             }
             return "Done!";
